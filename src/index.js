b3bd82ae584030b780bda5f2ffe6553c67f95eee
LESANF <54767632+LESANF@users.noreply.github.com>
2019-12-12 15:36:35 +0900

    ING

---
+++ src/index.js
@@ -0,0 +1,63 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+// import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+
+const plusBtn = document.querySelector("#js-plusBtn"),
+  minusBtn = document.querySelector("#js-minusBtn"),
+  multiBtn = document.querySelector("#js-multiBtn"),
+  divBtn = document.querySelector("#js-divBtn"),
+  text = document.querySelector("#js-number"),
+  button = document.querySelectorAll(".normal-btn"),
+  reset = document.querySelector("#js-resetBtn"),
+  result = document.querySelector("#js-resultBtn");
+
+let realone = 0;
+let realtwo = 0;
+let ans = 0;
+
+function handlereset() {
+  realone = 0;
+  realtwo = 0;
+  ans = 0;
+  text.innerText = 0;
+}
+
+reset.addEventListener("click", handlereset);
+
+function handleresult() {
+  ans = ans + realtwo;
+  text.innerText = ans;
+}
+
+result.addEventListener("click", handleresult);
+
+function getNum2(e) {
+  const two = parseInt(e.target.value);
+  realtwo = two;
+}
+
+function plusInto() {
+  Array.from(button).forEach(function(e) {
+    e.addEventListener("click", getNum2);
+  });
+}
+
+function plus() {
+  ans = realone;
+  plusInto();
+}
+
+plusBtn.addEventListener("click", plus);
+
+function getNum(e) {
+  const one = parseInt(e.target.value);
+  realone = one;
+}
+
+function init() {
+  Array.from(button).forEach(function(e) {
+    e.addEventListener("click", getNum);
+  });
+}
+
+init();
