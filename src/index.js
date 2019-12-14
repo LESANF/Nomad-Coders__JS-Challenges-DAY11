a1e817772c459506f544e94120eabbdba7aa11ae
LESANF <nagong1000@naver.com>
2019-12-15 02:23:29 +0900

    docs: Edit src\index.js to include New Comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import "./styles.css";
 
-const result = document.querySelector(".js-result");
-const reset = document.querySelector(".js-reset");
-const equals = document.querySelector(".js-equals");
-const numbers = Array.from(document.querySelectorAll(".js-number"));
-const operations = Array.from(document.querySelectorAll(".js-operation"));
+const result = document.querySelector(".js-result"); // 보여지는창
+const reset = document.querySelector(".js-reset"); // C
+const equals = document.querySelector(".js-equals"); // =
+const numbers = Array.from(document.querySelectorAll(".js-number")); // NUM
+const operations = Array.from(document.querySelectorAll(".js-operation")); //+,-,*,/
 
 let firstValue = "",
   firstDone,
@@ -32,6 +32,10 @@ function doOperation() {
 function handleNumberClick(e) {
   const clickedNum = e.target.innerText;
   if (!firstDone) {
+    // firstDone은 전역 변수로 선언만 되어있다
+    //if(firstDone)의 의미는 firstDone에 값이 있으면 조건 true이고
+    //다음과 같이 !firstDone는 firstDone에 값이 없을경우에 조건문에서 true이다.
+    //즉 firstDone에 아무것도 없으면 내용에 있는 코드가 실행된다.
     firstValue = firstValue + clickedNum;
     result.innerHTML = firstValue;
   } else {
@@ -76,10 +80,12 @@ function handleEqualsClick() {
 }
 
 numbers.forEach(function(number) {
+  // 숫자누르기
   number.addEventListener("click", handleNumberClick);
 });
 operations.forEach(function(operation) {
+  // 연산버튼 누르기
   operation.addEventListener("click", handleOperationClick);
 });
-reset.addEventListener("click", handleReset);
-equals.addEventListener("click", handleEqualsClick);
+reset.addEventListener("click", handleReset); // AC버튼
+equals.addEventListener("click", handleEqualsClick); // = 버튼
